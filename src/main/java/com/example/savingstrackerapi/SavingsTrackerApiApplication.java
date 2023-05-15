f59a206c54a1f68bc8343646156353f48d6c82c1
Krzykoz <K.Krzykoz@Gmail.com>
2023-05-15 15:54:55 +0200

    Project Creation

---
+++ src/main/java/com/example/savingstrackerapi/SavingsTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.savingstrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingsTrackerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SavingsTrackerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/savingstrackerapi/SavingsTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.savingstrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingsTrackerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
