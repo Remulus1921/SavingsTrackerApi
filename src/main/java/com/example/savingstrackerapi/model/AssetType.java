92f80ce1c4df589b9055fd3daa695af6b08a7626
Remigiusz Bekierz <remobekierz@onet.pl>
2023-05-18 15:07:57 +0200

    Database structure implemented

---
+++ src/main/java/com/example/savingstrackerapi/model/AssetType.java
@@ -0,0 +1,27 @@
+package com.example.savingstrackerapi.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "assets_type")
+public class AssetType {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+  private String name;
+
+  @OneToMany(targetEntity = Asset.class, cascade = CascadeType.ALL)
+  @JoinColumn(name = "type", referencedColumnName = "id")
+  List<Asset> assetList;
+}
